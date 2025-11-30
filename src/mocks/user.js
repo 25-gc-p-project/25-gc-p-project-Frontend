a272dc2bc70b919c10dbd2899b00e8244dd87ebe
unknown <1022gusl@gmail.com>
2025-11-30 18:17:26 +0900

    feat: 마이페이지 프로필 업데이트 임시 구현

--- src/mocks/user.js
+++ src/mocks/user.js
@@ -1,8 +1,10 @@
 export const demoId = "demoUser";
-export const demoPassword = "password123";
+export const demoPassword = "password123@";
 
 export const demoUser = {
-  id: 1,
-  username: demoId,
+  id: demoId,
   name: "데모 유저",
+  phone: "010-1234-5678",
+  address: "서울시 강남구 테헤란로 123",
+  birth: "2000-01-01",
 };
